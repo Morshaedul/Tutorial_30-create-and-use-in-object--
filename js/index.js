fc12cd72d6b0b68ffd26e2a74ef9a4634a6f6571
Morshaedul <morshaedulanirbanit@gmail.com>
2022-04-24 19:51:46 +0600

    first commit

---
+++ js/index.js
@@ -0,0 +1,120 @@
+// var names=["Ashraful", "Sakib","Mushfiq", "Mahamudullah", "Mashrafee"]
+// for(i=0; i<5; i++){
+//   console.log(names[i])
+// }
+
+
+// var num= parseInt(prompt("Enter your number:"))
+
+// if( num<0 || num>100){
+//   console.log("Your mark is invalid")
+// }
+// else if(num>=80 && num<=100){
+//   console.log("Your gread is : A+")
+// }
+// else if(num>=70 && num<=79){
+//   console.log("Your gread is : A")
+// }
+// else if(num>=60 && num<=69){
+//   console.log("Your gread is : A-")
+// }
+// else if(num>=50 && num<=59){
+//   console.log("Your gread is : B")
+// }
+// else if(num>=40 && num<=49){
+//   console.log("Your gread is : C")
+// }
+// else if(num>=33 && num<=39){
+//   console.log("Your gread is : D")
+// }
+// else{
+//   console.log("Tumi fail : F")
+// }
+
+
+//create a program called highestScore//
+
+// function highestScore(scores){
+//     var max=[0]
+//     for (x=1; x<scores.length; x++){
+//       if(max<scores[x]){
+//         max=scores[x]
+//       }
+//     }
+//       return max;
+//     }
+//     var num=[20,30,50,80,10]
+//     var maxScore=highestScore(num)
+//     console.log(maxScore)
+
+
+//create and use in object.
+
+// function student(name,age,cgpa,lang){
+//   this.name=name
+//   this.age=age
+//   this.cgpa=cgpa
+//   this.lang=lang
+  
+//   this.display=function(){
+    
+//     document.write(this.name)
+//     document.write(this.age)
+//     document.write(this.cgpa)
+//     document.write(this.lang)
+  
+//   }
+// }
+// var student1=new student("MORSHAEDUL ALOM", 20, 5.00, ["bangla", "english","NONE"])
+// var student2=new student("SABRINA ALOM", 20, 4.00, ["bangla", "english","ALL"])
+// var student3=new student("SABIB SINAN", 2, 5.00, ["bangla", "english","ICT"])
+
+// student1.display()
+// student2.display()
+// student3.display()
+
+// var num1=parseInt(prompt("Enter your frist number=:"));
+// var num2=parseInt(prompt("Enter your second number=:"));
+// var result= Math.max(num1,num2)
+// document.write(result)
+
+// function Student(name,age,cgpa,lang){
+//     this.name=name
+//     this.age=age
+//     this.cgpa=cgpa
+//     this.lang=lang
+
+//     this.display=function(){
+//         console.log(this.name)
+//         console.log(this.age)
+//         console.log(this.cgpa)
+//         console.log(this.lang)
+//     }
+// }
+// var student1= new Student( "morshaedul",24, 3.50,["bangla","english","math"] )
+// var student2= new Student( "sabrina",24, 3.50,["midwifary","english","physics"] )
+// var student3= new Student( "sabib",12, 5.00,["english","math","chemistry"] )
+// student1.display()
+// student2.display()
+// student3.display()
+
+
+// Math.floor(Math.random()*5)
+
+// function highestScore(scores){
+//     var max=[0]
+//     for (x=1; x<scores.length; x++){
+//       if(max<scores[x]){
+//         max=scores[x]
+//       }
+//     }
+//       return max;
+//     }
+//     var num=[20,30,50,80,10]
+//     var maxScore=highestScore(num)
+//     console.log(maxScore)
+
+
+
+
+
